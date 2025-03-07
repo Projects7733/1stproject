cfa3b114ade705f8597282b0791377bfa373198b
yuvaraj7733 <162885054+Projects7733@users.noreply.github.com>
2025-03-07 10:20:53 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,5 +2,5 @@ pip install chaterbot
 pip install chatterbot_corpus
 chatbot = chatBot('My chatBot')
 trainer = chatterBotCorpusTrainer(chatbot)
-print("hello world")
-print("chatbot")
+print("hello world");
+print("chatbot");
